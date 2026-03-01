3f0227c25408e92096654b9a92692e989b7c92cd
devdriven <git@devdriven.com>
2026-03-01 04:27:49 -0600

    scripts/before-commit.sh: check syntax after striping execution_count.

--- scripts/before-commit.sh
+++ scripts/before-commit.sh
@@ -3,8 +3,16 @@
 set -x
 find c101 -name '*.ipynb' | while read nb
 do
+  nb_bak="$nb.bak"
   py="${nb%.*}.py"
   sed -E -i.bak -e 's~^( *"execution_count":)[^,]*(.*)~\1 null\2~' "$nb"
+  if not python -m py_compile "$nb"
+  then
+    mv "$nb_bak" "$nb"
+    exit 9
+  else
+    rm -f "$nb_bak"
+  fi
   chmod u+w "$py" || :
   jupyter nbconvert --to script "$nb"
   chmod -w "$py"
