c520f5b1bffa8929fb500f02f7312580c681ede0
devdriven <git@devdriven.com>
2026-03-01 02:37:52 -0600

    Checkpoint.

--- before-commit.sh
+++ before-commit.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 set -x
-sed -E -i.bak -e 's~^( *"execution_count":)[^,]*(.*)~\1 null\2~' combinators-101.ipynb
-jupyter nbconvert --to script combinators-101.ipynb
+for nb in *.ipynb
+do
+  sed -E -i.bak -e 's~^( *"execution_count":)[^,]*(.*)~\1 null\2~' "$nb"
+  jupyter nbconvert --to script "$nb"
+done
