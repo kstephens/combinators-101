c88081f8e470949136e3236ce6cc774bc5f283b4
devdriven <git@devdriven.com>
2026-03-01 04:54:11 -0600

    Build html/.

--- scripts/before-commit.sh
+++ scripts/before-commit.sh
@@ -4,10 +4,18 @@ set -x
 find c101 -name '*.ipynb' | while read nb
 do
   py="${nb%.*}.py"
+  html="${nb%.*}.html"
+  html_dst="html/$html"
 
   nb_bak="$nb.bak"
   sed -E -i.bak -e 's~^( *"execution_count.":)[^,]*(.*)~\1 null\2~' "$nb"
   jq -e < "$nb" >/dev/null || exit $?
+
+  jupyter nbconvert --to html --execute "$nb"
+  mkdir -p "${html_dst%/*}"
+  chmod u+w "$html_dst" || :
+  mv "$html" "$html_dst"
+  chmod -w "$html"
 
   chmod u+w "$py" || :
   jupyter nbconvert --to script "$nb"
