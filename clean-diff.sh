77accc80e3ca43318ee16351f891995b5d216ee5
devdriven <git@devdriven.com>
2026-02-28 21:43:18 -0600

    clean-diff.sh.

--- clean-diff.sh
+++ clean-diff.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -x
-sed -E -e 's~^( *"execution_count":)[^,]*~\1: null,~' -i combinators-101.ipynb
+sed -E -i.bak -e 's~^( *"execution_count":)[^,]*(.*)~\1 null\2~' combinators-101.ipynb
